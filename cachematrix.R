2b7fbf87d107d31fc2f7caaa2881e56e46cdeb33
zelmarou <zelmarou@gmail.com>
2015-02-21 22:10:32 +0100

    Coursera Programming Assignment 2
    This commit contains the implementation of the 2 functions:
    makeCacheMatrix, cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# This function creates CacheMatrix Object
+# For instance to create a random CacheMatrix of 10 rows and 10 columns just type:
+# myMatrix <- makeCacheMatrix(matrix(rexp(100, rate=.1), ncol=10))
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+# This function computes, and stores the solve of an object of type makeCacheMatrix
+# For instance to solve your previously created matrix just type
+# cacheSolve(myMatrix)
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
